b3d30efc8ffbb681e7e9b24863e9e07c42b07335
mahakatas <mahakatas@gmail.com>
2023-07-14 13:13:15 +0200

    defined endpoints for patients weights

---
+++ src/main/java/com/zimttech/screening/controller/WeightController.java
@@ -0,0 +1,53 @@
+package com.zimttech.screening.controller;
+
+
+import com.zimttech.screening.domain.Weight;
+import com.zimttech.screening.services.WeightService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+
+@Controller
+@RequestMapping(path = "/screening")
+@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
+public class WeightController {
+    private final WeightService weightService;
+
+
+    @GetMapping(path = "/weights")
+    public @ResponseBody Iterable<Weight> getAllWeights() {
+
+        return weightService.fetchAllWeights();
+
+    }
+
+    @PostMapping(path = "/addWeight")
+    public @ResponseBody Boolean add(@RequestBody Weight weight) {
+        return weightService.addWeight(weight);
+    }
+
+    @RequestMapping(path = "/deleteWeight/{patientNumber}")
+    @ResponseBody
+    public Iterable<Weight> delete(@PathVariable String patientNumber) {
+
+        return weightService.deleteWeight(patientNumber);
+    }
+
+    @PutMapping("/updateWeight")
+    @ResponseBody
+    public Iterable<Weight> update(@RequestBody Weight weight) {
+        return weightService.updateWeight(weight);
+
+    }
+
+    @RequestMapping("/findWeight/{patientNumber}")
+    public Weight find(@PathVariable String patientNumber) {
+
+        return weightService.findWeight(patientNumber);
+    }
+
+}
