fbf86aacf608e1395df5491c13ad9e9db42b7cfb
mahakatas <mahakatas@gmail.com>
2023-07-14 13:39:55 +0200

    defined methods for managing patient data for HIV Treatment, BloodGlucose

--- src/main/java/com/zimttech/screening/services/BloodGlucoseService.java
+++ src/main/java/com/zimttech/screening/services/BloodGlucoseService.java
@@ -1,4 +1,15 @@
 package com.zimttech.screening.services;
 
+import com.zimttech.screening.domain.BloodGlucose;
+
 public interface BloodGlucoseService {
+    Iterable<BloodGlucose> fetchAllBGs();
+
+    Boolean addBloodGlucose(BloodGlucose bloodGlucose);
+
+    Iterable<BloodGlucose> deleteBloodGlucose(String patientNumber);
+
+    Iterable<BloodGlucose> updateBloodGlucose(BloodGlucose bloodGlucose);
+
+    BloodGlucose findBloodGlucose(String patientNumber);
 }
--- src/main/java/com/zimttech/screening/services/TreatmentDataService.java
+++ src/main/java/com/zimttech/screening/services/TreatmentDataService.java
@@ -1,4 +1,15 @@
 package com.zimttech.screening.services;
 
+import com.zimttech.screening.domain.TreatmentData;
+
 public interface TreatmentDataService {
+    Iterable<TreatmentData> deleteTreatmentData(String patientNumber) ;
+
+    Iterable<TreatmentData> fetchAllTreatments();
+
+    Boolean addTreatmentData(TreatmentData treatmentData);
+
+    Iterable<TreatmentData> updateTreatmentData(TreatmentData treatmentData);
+
+    TreatmentData findTreatmentData(String patientNumber);
 }
