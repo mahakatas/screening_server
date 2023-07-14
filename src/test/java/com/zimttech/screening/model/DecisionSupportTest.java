6b251869a2c9bca7b22ccac5242f2194d058d9c0
mahakatas <mahakatas@gmail.com>
2023-07-14 14:21:16 +0200

    added tests for DecisionSupport

---
+++ src/test/java/com/zimttech/screening/model/DecisionSupportTest.java
@@ -0,0 +1,24 @@
+package com.zimttech.screening.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecisionSupportTest {
+
+    @Test
+    void getBloodGlucoseCategory() {
+    }
+
+    @Test
+    void getBPCategory() {
+    }
+
+    @Test
+    void getBMICategory() {
+    }
+
+    @Test
+    void getHeightCategory() {
+    }
+}
---
+++ src/test/java/com/zimttech/screening/services/DecisionSupportServiceTest.java
@@ -0,0 +1,12 @@
+package com.zimttech.screening.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecisionSupportServiceTest {
+
+    @Test
+    void computeDecision() {
+    }
+}
