a83955620ec2d6cf58220e31675be38eebd460a9
mahakatas <mahakatas@gmail.com>
2023-07-14 13:40:22 +0200

    defined endpoints for managing patient data for HIV Treatment, BloodGlucose

---
+++ src/main/java/com/zimttech/screening/controller/BloodGlucoseController.java
@@ -0,0 +1,52 @@
+
+package com.zimttech.screening.controller;
+
+
+import com.zimttech.screening.domain.BloodGlucose;
+import com.zimttech.screening.services.BloodGlucoseService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping(path = "/screening")
+@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
+public class BloodGlucoseController {
+    private final BloodGlucoseService bloodGlucoseService;
+
+
+    @GetMapping(path = "/bloodGlucoses")
+    public @ResponseBody Iterable<BloodGlucose> getAllBloodGlucoses() {
+
+        return bloodGlucoseService.fetchAllBGs();
+
+    }
+
+    @PostMapping(path = "/addBP")
+    public @ResponseBody Boolean add(@RequestBody BloodGlucose bloodGlucose) {
+        return bloodGlucoseService.addBloodGlucose(bloodGlucose);
+    }
+
+    @RequestMapping(path = "/deleteBloodGlucose/{patientNumber}")
+    @ResponseBody
+    public Iterable<BloodGlucose> delete(@PathVariable String patientNumber) {
+
+        return bloodGlucoseService.deleteBloodGlucose(patientNumber);
+    }
+
+    @PutMapping("/updateBloodGlucose")
+    @ResponseBody
+    public Iterable<BloodGlucose> update(@RequestBody BloodGlucose bloodGlucose) {
+        return bloodGlucoseService.updateBloodGlucose(bloodGlucose);
+
+    }
+
+    @RequestMapping("/findBloodGlucose/{patientNumber}")
+    public BloodGlucose find(@PathVariable String patientNumber) {
+
+        return bloodGlucoseService.findBloodGlucose(patientNumber);
+    }
+
+}
---
+++ src/main/java/com/zimttech/screening/controller/TreatmentDataController.java
@@ -0,0 +1,52 @@
+
+package com.zimttech.screening.controller;
+
+
+import com.zimttech.screening.domain.TreatmentData;
+import com.zimttech.screening.services.TreatmentDataService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping(path = "/screening")
+@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
+public class TreatmentDataController {
+    private final TreatmentDataService treatmentDataService;
+
+
+    @GetMapping(path = "/treatmentData")
+    public @ResponseBody Iterable<TreatmentData> getAllTreatmentData() {
+
+        return treatmentDataService.fetchAllTreatments();
+
+    }
+
+    @PostMapping(path = "/addBP")
+    public @ResponseBody Boolean add(@RequestBody TreatmentData treatmentData) {
+        return treatmentDataService.addTreatmentData(treatmentData);
+    }
+
+    @RequestMapping(path = "/deleteTreatmentData/{patientNumber}")
+    @ResponseBody
+    public Iterable<TreatmentData> delete(@PathVariable String patientNumber) {
+
+        return treatmentDataService.deleteTreatmentData(patientNumber);
+    }
+
+    @PutMapping("/updateTreatmentData")
+    @ResponseBody
+    public Iterable<TreatmentData> update(@RequestBody TreatmentData treatmentData) {
+        return treatmentDataService.updateTreatmentData(treatmentData);
+
+    }
+
+    @RequestMapping("/findTreatmentData/{patientNumber}")
+    public TreatmentData find(@PathVariable String patientNumber) {
+
+        return treatmentDataService.findTreatmentData(patientNumber);
+    }
+
+}
