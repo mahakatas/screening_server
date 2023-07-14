acebebd91165acd94d3b2747fd14bbeb449c3075
mahakatas <mahakatas@gmail.com>
2023-07-14 13:12:45 +0200

    defined a set of methods for managing patient weights

--- src/main/java/com/zimttech/screening/services/WeightService.java
+++ src/main/java/com/zimttech/screening/services/WeightService.java
@@ -1,4 +1,14 @@
 package com.zimttech.screening.services;
 
+import com.zimttech.screening.domain.Weight;
+
 public interface WeightService {
+    Iterable<Weight> fetchAllWeights();
+
+    Boolean addWeight(Weight weight);
+
+    Iterable<Weight> deleteWeight(String patientNumber);
+    Iterable<Weight> updateWeight(Weight weight);
+
+    Weight findWeight(String patientNumber);
 }
