8408f89cebd926b4293d6e5184bf7ab6f219a7ca
mahakatas <mahakatas@gmail.com>
2023-07-14 12:29:21 +0200

    added blueprint for fetching patients

--- src/main/java/com/zimttech/screening/services/PatientService.java
+++ src/main/java/com/zimttech/screening/services/PatientService.java
@@ -1,4 +1,9 @@
 package com.zimttech.screening.services;
 
+import com.zimttech.screening.domain.Patient;
+
 public interface PatientService {
+    default Iterable<Patient> fetchAllPatients() {
+        return null;
+    }
 }
