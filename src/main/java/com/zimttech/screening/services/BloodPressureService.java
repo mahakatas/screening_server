8834597196b9ad4aff349dc2fe0ee5dc376a272d
mahakatas <mahakatas@gmail.com>
2023-07-14 13:28:01 +0200

    defined methods for managing patient for BloodPressure, Height

--- src/main/java/com/zimttech/screening/services/BloodPressureService.java
+++ src/main/java/com/zimttech/screening/services/BloodPressureService.java
@@ -1,4 +1,15 @@
 package com.zimttech.screening.services;
 
+import com.zimttech.screening.domain.BloodPressure;
+
 public interface BloodPressureService {
+    Iterable<BloodPressure> fetchAllBPs();
+
+    Boolean addBloodPressure(BloodPressure bloodPressure);
+
+    Iterable<BloodPressure> deleteBloodPressure(String patientNumber);
+
+    Iterable<BloodPressure> updateBloodPressure(BloodPressure bloodPressure);
+
+    BloodPressure findBloodPressure(String patientNumber);
 }
--- src/main/java/com/zimttech/screening/services/HeightService.java
+++ src/main/java/com/zimttech/screening/services/HeightService.java
@@ -1,4 +1,15 @@
 package com.zimttech.screening.services;
 
+import com.zimttech.screening.domain.Height;
+
 public interface HeightService {
+    Iterable<Height> fetchAllHeights();
+
+    Height findHeight(String patientNumber);
+
+    Iterable<Height> updateHeight(Height height);
+
+    Iterable<Height> deleteHeight(String patientNumber);
+
+    Boolean addHeight(Height height);
 }
