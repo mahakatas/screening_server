df2af6ad750d385d565acfc488e4c492b48a49c8
mahakatas <mahakatas@gmail.com>
2023-07-14 12:56:04 +0200

    defined end points for patient data through PatientController

---
+++ src/main/java/com/zimttech/screening/controller/PatientController.java
@@ -0,0 +1,53 @@
+package com.zimttech.screening.controller;
+
+
+import com.zimttech.screening.domain.Patient;
+import com.zimttech.screening.services.PatientService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+
+@Controller
+@RequestMapping(path = "/screening")
+@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
+public class PatientController {
+    private final PatientService patientService;
+
+
+    @GetMapping(path = "/patients")
+    public @ResponseBody Iterable<Patient> getAllPatients() {
+
+        return patientService.fetchAllPatients();
+
+    }
+
+    @PostMapping(path = "/addPatient")
+    public @ResponseBody Boolean add(@RequestBody Patient patient) {
+        return patientService.addPatient(patient);
+    }
+
+    @RequestMapping(path = "/deletePatient/{patientNumber}")
+    @ResponseBody
+    public Iterable<Patient> delete(@PathVariable String patientNumber) {
+
+        return patientService.deletePatient(patientNumber);
+    }
+
+    @PutMapping("/updatePatient/{patientNumber}")
+    @ResponseBody
+    public Iterable<Patient> update(@PathVariable String patientNumber, @RequestBody Patient patient) {
+        return patientService.updatePatient(patientNumber, patient);
+
+    }
+
+    @RequestMapping("/findPatient/{patientNumber}")
+    public Patient find(@PathVariable String patientNumber) {
+
+        return patientService.findPatient(patientNumber);
+    }
+
+}
