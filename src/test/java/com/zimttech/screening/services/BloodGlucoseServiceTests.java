f5e493b7a07a71774e5ffc8a4e016063201ee350
mahakatas <mahakatas@gmail.com>
2023-07-14 14:22:16 +0200

    added tests for Blood Glucose

---
+++ src/test/java/com/zimttech/screening/services/BloodGlucoseServiceTests.java
@@ -0,0 +1,28 @@
+package com.zimttech.screening.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BloodGlucoseServiceTests {
+
+    @Test
+    void fetchAllBGs() {
+    }
+
+    @Test
+    void addBloodGlucose() {
+    }
+
+    @Test
+    void deleteBloodGlucose() {
+    }
+
+    @Test
+    void updateBloodGlucose() {
+    }
+
+    @Test
+    void findBloodGlucose() {
+    }
+}
